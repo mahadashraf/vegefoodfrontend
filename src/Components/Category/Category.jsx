1ba64fd9549f9f9c5c9031edd3b798592e7db130
Mahad Ashraf <mahadashraf.850@gmail.com>
2024-09-05 11:28:31 +0500

    updated api

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -26,15 +26,15 @@ export const Category = () => {
       return () => clearInterval(interval);
     }
   }, [products]);
+
   const fetchProductData = () => {
-    axios.get('https://vegefoodbackend.vercel.app/api/getImage')
+    axios.get('http://localhost:5100/getImage')
       .then(res => {
         setProducts(res.data);
         setDisplayedProducts(getRandomProducts(res.data));
       })
       .catch(err => console.log('Fetch Error:', err));
   };
-  
 
   return (
     <div className="category-section">
